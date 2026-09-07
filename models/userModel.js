789311ee88d08e735fc9e1ac6d9673a079b65654
Prabhleen kaur <prabhleenkaur9946@gmail.com>
2026-09-07 14:40:52 +0300

    feat(models): add userModel addOne()

--- models/userModel.js
+++ models/userModel.js
@@ -6,3 +6,30 @@ module.exports = {};
 const getAll = () => {
     return userArray;
 };
+const addOne = (
+    name,
+    email,
+    password,
+    phone_number,
+    gender,
+    date_of_birth,
+    membership_status
+) => {
+    if (!name || !email || !password || !phone_number || !gender || !date_of_birth || !membership_status) {
+        return false;
+    }
+
+    const newUser = {
+        id: nextId++,
+        name,
+        email,
+        password,
+        phone_number,
+        gender,
+        date_of_birth,
+        membership_status,
+    };
+
+    userArray.push(newUser);
+    return newUser;
+};
