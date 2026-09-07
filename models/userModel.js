38aa7753ed2ca128c7baaf6509bd2f3b43f3b17e
Prabhleen kaur <prabhleenkaur9946@gmail.com>
2026-09-07 14:48:16 +0300

    feat(models): add userModel deleteOneById()

--- models/userModel.js
+++ models/userModel.js
@@ -34,20 +34,28 @@ const addOne = (
     return newUser;
 };
 const findById = (id) => {
-   const user = userArray.find((u) => u.id === Number(id));
-   return user || false;
+    const user = userArray.find((u) => u.id === Number(id));
+    return user || false;
 };
 const updateOneById = (id, updatedData) => {
-   const user = findById(id);
-   if (!user) return false;
+    const user = findById(id);
+    if (!user) return false;
 
-   if (updatedData.name) user.name = updatedData.name;
-   if (updatedData.email) user.email = updatedData.email;
-   if (updatedData.password) user.password = updatedData.password;
-   if (updatedData.phone_number) user.phone_number = updatedData.phone_number;
-   if (updatedData.gender) user.gender = updatedData.gender;
-   if (updatedData.date_of_birth) user.date_of_birth = updatedData.date_of_birth;
-   if (updatedData.membership_status) user.membership_status = updatedData.membership_status;
+    if (updatedData.name) user.name = updatedData.name;
+    if (updatedData.email) user.email = updatedData.email;
+    if (updatedData.password) user.password = updatedData.password;
+    if (updatedData.phone_number) user.phone_number = updatedData.phone_number;
+    if (updatedData.gender) user.gender = updatedData.gender;
+    if (updatedData.date_of_birth) user.date_of_birth = updatedData.date_of_birth;
+    if (updatedData.membership_status) user.membership_status = updatedData.membership_status;
 
-   return user;
+    return user;
+};
+const deleteOneById = (id) => {
+    const existing = findById(id);
+    if (!existing) return false;
+
+    const initialLength = userArray.length;
+    userArray = userArray.filter((u) => u.id !== Number(id));
+    return userArray.length < initialLength;
 };
