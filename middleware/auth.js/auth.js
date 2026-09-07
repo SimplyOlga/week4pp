e5e50c093e3cafff8e7661b2c6742370ccaafe69
Prabhleen kaur <prabhleenkaur9946@gmail.com>
2026-09-07 16:01:30 +0300

    error

---
+++ middleware/auth.js/auth.js
@@ -0,0 +1,12 @@
+module.exports = (req, res, next) => {
+    next();
+};
+module.exports = (req, res, next) => {
+    const isAdmin = req.query.admin === 'true';
+
+    if (!isAdmin) {
+        return res.status(403).json({ message: 'Admin access required' });
+    }
+
+    next();
+};
--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const { getAllTours } = require('../controllers/tourControllers');
+const auth = require('../middleware/auth');
 
 router.get('/', getAllTours);
+router.get('/:tourId', getTourById);
+
+router.use(auth);
+
+router.post('/', createTour);
+router.put('/:tourId', updateTour);
+router.delete('/:tourId', deleteTour);
 
 module.exports = router;
