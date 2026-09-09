d3d44b612e7aec459d9a1f6a0673e317ab06fa9a
Prabhleen kaur <prabhleenkaur9946@gmail.com>
2026-09-09 14:33:30 +0300

    feat(components): add servicesData state to Services component

--- WEDNESDAY/week4-fe-pp/src/components/Services.jsx
+++ WEDNESDAY/week4-fe-pp/src/components/Services.jsx
@@ -1,8 +1,23 @@
 import { services } from '../data'
 import Title from './Title'
 import Service from './Service'
+import { useState } from 'react';
 
 const Services = () => {
+  function Services() {
+    const [servicesData, setServicesData] = useState(services);
+
+    return (
+      <section className='section services' id='services'>
+        <Title title='our' subTitle='services' />
+        <div className='section-center services-center'>
+          {services.map((service) => {  // Still using old 'services'
+            return <Service {...service} key={service.id} />
+          })}
+        </div>
+      </section>
+    )
+  }
   return (
     <section className='section services' id='services'>
       <Title title='our' subTitle='services' />
