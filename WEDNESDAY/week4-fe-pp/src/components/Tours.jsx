7a951c0604ad9f46bef048308f70690cdd2a1552
Prabhleen kaur <prabhleenkaur9946@gmail.com>
2026-09-09 15:15:34 +0300

    refactor(components): pass removeTour handler to Tour components

--- WEDNESDAY/week4-fe-pp/src/components/Tours.jsx
+++ WEDNESDAY/week4-fe-pp/src/components/Tours.jsx
@@ -5,14 +5,15 @@ import Tour from "./Tour";
 
 function Tours() {
   const [toursData, setToursData] = useState(tours);
-  
+
   return (
     <section className="section" id="tours">
       <Title title="featured" subTitle="tours" />
 
       <div className="section-center featured-center">
         {toursData.map((tour) => {
-          return <Tour {...tour} key={tour.id} />
+          return <Tour {...tour} key={tour.id}
+            onRemove={handleRemoveTour} />
         })}
       </div>
     </section>
